24124ee409aea141dbd4ebf893751a82ee63a1e7
Martin Goldhahn <martin.goldhahn@uis.no>
2013-05-28 15:46:00 +0200

    rename web param from faculties to faculty

--- src/main/java/no/uis/service/ws/studinfosolr/StudinfoSolrService.java
+++ src/main/java/no/uis/service/ws/studinfosolr/StudinfoSolrService.java
@@ -13,14 +13,14 @@ public interface StudinfoSolrService {
       @WebParam(name="solrType") SolrType solrType) throws SolrUpdateException;
 
   void updateSolrEmne(
-      @WebParam(name="faculties") int[] faculties,
+      @WebParam(name="faculty") int[] faculties,
       @WebParam(name="year") int year, 
       @WebParam(name="semester") String semester, 
       @WebParam(name="language") String language,
       @WebParam(name="solrType") SolrType solrType) throws SolrUpdateException;
 
   void updateSolrStudieprogram(
-      @WebParam(name="faculties") int[] faculties,
+      @WebParam(name="faculty") int[] faculties,
       @WebParam(name="year") int year, 
       @WebParam(name="semester") String semester, 
       @WebParam(name="language") String language,
